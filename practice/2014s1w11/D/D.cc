ca2800c8ba89b784accc7f11fa69bb086d69a071
The Vy Hong <tvho795@cse.unsw.edu.au>
2014-06-03 18:10:42 +1000

    Add w11 D & E

--- practice/2014s1w11/D/D.cc
+++ practice/2014s1w11/D/D.cc
@@ -4,7 +4,7 @@
 using namespace std;
 
 const int N = 2014;
-const int M = N*N;
+const int M = 2*1e6+5;
 int n, m;
 int inv(int i) {
   return (i + n) % (2*n);
@@ -25,42 +25,42 @@ void add_edge(int u, int v) {
 
 int seen[N], seen_r[N], scc[N], postorder[N], p;
 
-void dfs(int u) { 
-  if (seen[u]) return; 
-  seen[u] = true; 
-  for (int e = start[u]; ~e; e = succ[e]) { 
-    dfs(to[e]); 
-  } 
-  postorder[p++] = u; 
-} 
+void dfs(int u) {
+  if (seen[u]) return;
+  seen[u] = true;
+  for (int e = start[u]; ~e; e = succ[e]) {
+    dfs(to[e]);
+  }
+  postorder[p++] = u;
+}
 
-void dfs_r(int u, int mark) { 
-  if (seen_r[u]) return; 
-  seen_r[u] = true; 
-  scc[u] = mark; 
-  for (int e = start_r[u]; ~e; e = succ_r[e]) { 
-    dfs_r(to_r[e], mark); 
-  } 
-} 
+void dfs_r(int u, int mark) {
+  if (seen_r[u]) return;
+  seen_r[u] = true;
+  scc[u] = mark;
+  for (int e = start_r[u]; ~e; e = succ_r[e]) {
+    dfs_r(to_r[e], mark);
+  }
+}
 
 int compute_sccs() {
   memset(seen, 0, sizeof seen);
   memset(seen_r, 0, sizeof seen_r);
   p = 0;
-  int sccs = 0; 
-  for (int i = 1; i <= n; i++) { 
-    if (!seen[i]) { 
-      dfs(i); 
-    } 
-  } 
-  for (int i = p - 1; i >= 0; i--) { 
-    int u = postorder[i]; 
-    if (!seen_r[u]) { 
-      dfs_r(u, sccs++); 
-    } 
-  } 
-  return sccs; 
-} 
+  int sccs = 0;
+  for (int i = 0; i < 2*n; i++) {
+    if (!seen[i]) {
+      dfs(i);
+    }
+  }
+  for (int i = p - 1; i >= 0; i--) {
+    int u = postorder[i];
+    if (!seen_r[u]) {
+      dfs_r(u, sccs++);
+    }
+  }
+  return sccs;
+}
 
 void init() {
   E = 0;
@@ -70,7 +70,7 @@ void init() {
 }
 
 int main() {
-  while(scanf("%d %d", &n, &m) != EOF) {
+  while(scanf("%d %d", &n, &m) == 2) {
     init();
     for (int i=0; i<m; i++) {
       int i1, i2;
@@ -78,27 +78,24 @@ int main() {
       scanf(" %c", &ch);
       bool flag1 = (ch == '+');
       scanf("%d", &i1);
-      i1--;
+      --i1;
       scanf(" %c", &ch);
       bool flag2 = (ch == '+');
       scanf("%d", &i2);
-      i2--;
+      --i2;
+      //printf("read %d %d\n", i1, i2);
       if (flag1 && flag2) {
-        //add_edge(inv(i1), i2);
-        add_edge(i1, inv(i2));
-        add_edge(i2, inv(i1));
-      } else if (!flag1 && !flag2) {
-        //add_edge(i1, inv(i2));
         add_edge(inv(i1), i2);
         add_edge(inv(i2), i1);
+      } else if (!flag1 && !flag2) {
+        add_edge(i1, inv(i2));
+        add_edge(i2, inv(i1));
       } else if (flag1 && !flag2) {
-        //add_edge(inv(i1), inv(i2));
-        add_edge(i1, i2);
-        add_edge(inv(i2), inv(i1));
-      } else {
-        //add_edge(i1, i2);
         add_edge(inv(i1), inv(i2));
         add_edge(i2, i1);
+      } else {
+        add_edge(i1, i2);
+        add_edge(inv(i2), inv(i1));
       }
     }
     compute_sccs();
@@ -106,6 +103,11 @@ int main() {
     for (int i=0; i<n; i++) {
       if (scc[i] == scc[inv(i)]) good = false;
     }
+    /*
+    for (int i = 0; i<n; i++) {
+      printf("scc[%d] = %d, scc[%d] = %d\n", i, scc[i], inv(i), scc[inv(i)]);
+    }
+    */
     printf("%d\n", (good) ? 1 : 0);
   }
 
---
+++ practice/2014s1w11/E/E.cc
@@ -0,0 +1,155 @@
+#include <cstdio>
+#include <stack>
+#include <cassert>
+#include <cstring>
+#include <algorithm>
+#include <utility>
+
+#define x first
+#define y second
+#define mp make_pair
+
+#define SUM 45
+#define PROD 362880
+
+#define r(k)  (k/9)
+#define c(k)  (k%9)
+#define sg(k) ((k/27)*3 + (k%27%9)/3)
+using namespace std;
+const int N = 81;
+
+typedef struct group {
+  int sum, prod;
+  int mask[10];
+} group;
+
+group rows[9], cols[9], subgrids[9];
+char board[100], sol[100];
+bool done;
+
+void init() {
+  for (int i=0; i<9; i++) {
+    rows[i].sum = 0;
+    rows[i].prod = 1;
+    memset(rows[i].mask, 0, sizeof rows[i].mask);
+
+    cols[i].sum = 0;
+    cols[i].prod = 1;
+    memset(cols[i].mask, 0, sizeof cols[i].mask);
+
+    subgrids[i].sum = 0;
+    subgrids[i].prod = 1;
+    memset(subgrids[i].mask, 0, sizeof subgrids[i].mask);
+  }
+  done = false;
+}
+
+void recurse(int k) {
+  //printf("k=%d\n", k);
+  //printf("%s\n", sol);
+  if (k == N) {
+    done = true;
+    printf("%s\n", sol);
+  }
+  if (board[k] == '.') {
+    int i = r(k), j = c(k), l = sg(k);
+    for (int v = 9; v>=1 && !done; v--) {
+      if (rows[i].mask[v] || cols[j].mask[v] || subgrids[l].mask[v])
+        continue;
+      /*
+      if (v+rows[i].sum > SUM || v*rows[i].prod > PROD ||
+          v+cols[j].sum > SUM || v*cols[j].prod > PROD ||
+          v+subgrids[l].sum > SUM || v*subgrids[l].prod > PROD) {
+        continue;
+      */
+      //rows[i].sum += v;
+      //rows[i].prod *= v;
+      rows[i].mask[v] = 1;
+      //cols[j].sum += v;
+      //cols[j].prod *= v;
+      cols[j].mask[v] = 1;
+      //subgrids[l].sum += v;
+      //subgrids[l].prod *= v;
+      subgrids[l].mask[v] = 1;
+      sol[k] = v+'0';
+
+      recurse(k+1);
+      //printf("back to %d\n", k);
+
+      //rows[i].sum -= v;
+      //rows[i].prod /= v;
+      rows[i].mask[v] = 0;
+      //cols[j].sum -= v;
+      //cols[j].prod /= v;
+      cols[j].mask[v] = 0;
+      //subgrids[l].sum -= v;
+      //subgrids[l].prod /= v;
+      subgrids[l].mask[v] = 0;
+      sol[k] = '.';
+    }
+  } else {
+    if (!done)
+      recurse(k+1);
+  }
+}
+
+void nonre() {
+  stack<pair<int, int> > stack;
+  stack.push(mp(0, 0));
+  while (!stack.empty()) {
+    pair<int, int> st = stack.top();
+    stack.pop();
+    int k = st.x;
+    //printf("k=%d, st.y = %d\n", k, st.y);
+    if (k == N) break;
+    if (board[k] != '.') {
+      stack.push(mp(k+1, 0)); // ignore this state
+      continue;
+    }
+    int i = r(k), j = c(k), l = sg(k);
+    if (st.y != 0) {
+      /* unfold stuffs */
+      rows[i].mask[st.y] = 0;
+      cols[j].mask[st.y] = 0;
+      subgrids[l].mask[st.y] = 0;
+    }
+    for (int v = st.y+1; v<=9; v++) {
+      if (rows[i].mask[v] || cols[j].mask[v] || subgrids[l].mask[v])
+        continue;
+      rows[i].mask[v] = 1;
+      cols[j].mask[v] = 1;
+      subgrids[l].mask[v] = 1;
+      sol[k] = v + '0';
+      stack.push(mp(k, v));   // store myself
+      stack.push(mp(k+1, 0));   // command the next one
+      break;
+    }
+  }
+  printf("%s\n", sol);
+}
+
+int main() {
+  while (scanf("%s", &board), strcmp(board, "end") != 0) {
+    assert(strlen(board) == N);
+    strncpy(sol, board, 100);
+    init();
+    for (int k=0; k<N; k++) {
+      if (board[k] == '.') continue;
+      int num = board[k] - '0';
+      int i = r(k), j = c(k), l = sg(k);
+      //printf("k=%d, i=%d, j=%d, l=%d\n", k, i, j, l);
+      //rows[i].sum += num;
+      //rows[i].prod *= num;
+      rows[i].mask[num] = 1;
+      //cols[j].sum += num;
+      //cols[j].prod *= num;
+      cols[j].mask[num] = 1;
+      //subgrids[l].sum += num;
+      //subgrids[l].prod *= num;
+      subgrids[l].mask[num] = 1;
+    }
+    //recurse(0);
+    nonre();
+  }
+  return 0;
+}
